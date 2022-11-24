f07942c14467ac36967816b448b2222a8b60aaf8
Norly Ayque <110006689+NorlyAyque@users.noreply.github.com>
2022-11-24 18:14:26 +0800

    Count Notif

--- Connection.php
+++ Connection.php
@@ -29,4 +29,18 @@ $CIT_Full = strtoupper("College of Industrial Technology");
 
 $CEAFA = strtoupper("CEAFA");
 $CEAFA_Full = strtoupper("College of Engineering, Architecture and Fine Arts");
+
+
+
+$QueryProp = ("SELECT COUNT(*) as TotalProposal FROM create_alangilan WHERE ProjectStatus = 'PENDING'");
+$CMDProp = $con->query($QueryProp) or die("Error Fethcing Data");
+while($ResultProp = mysqli_fetch_array($CMDProp)){$CountProposal = $ResultProp['TotalProposal'];}
+
+$QueryEval = ("SELECT COUNT(*) as TotalEvaluation FROM evaluation_alangilan WHERE ProjectStatus = 'PENDING'");
+$CMDEval = $con->query($QueryEval) or die("Error Fethcing Data");
+while($ResultEval = mysqli_fetch_array($CMDEval)){$CountEvaluation = $ResultEval['TotalEvaluation'];}
+
+$QueryMon = ("SELECT COUNT(*) as TotalMonitoring FROM monitoring_alangilan WHERE Remarks = 'PENDING'");
+$CMDMon = $con->query($QueryMon) or die("Error Fethcing Data");
+while($ResultMon = mysqli_fetch_array($CMDMon)){$CountMonitoring = $ResultMon['TotalMonitoring'];}
 ?>
